1b6f50f3d8118959636841b12c603ce3fab3b19a
samssi <samir.hamadi@gmail.com>
2023-12-02 19:25:53 +0200

    Move to window_manager

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use glfw::Action;
-use glfw::{Context, Key};
-
 mod eventsource;
 mod graphics;
+mod os;
 
 use std::collections::HashMap;
 
 use crate::eventsource::source::Message;
 use crate::eventsource::source::GEContext;
+use crate::os::window_manager::start_window_manager;
 
 #[allow(dead_code)]
 fn update() {
@@ -39,46 +38,6 @@ fn update() {
     println!("hashmap: {:?}", context.source);
 }
 
-fn report_run_count(count: i32) {
-    println!("Running... Count {}", count)
-}
-
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 600;
-
 fn main() {
-    use glfw::fail_on_errors;
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
-
-    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
-
-    let (mut window, events) =
-        glfw.create_window(SCREEN_WIDTH, SCREEN_HEIGHT, "GamEngine", glfw::WindowMode::Windowed)
-            .expect("Failed to create GLFW window");
-
-        window.make_current();
-        window.set_key_polling(true);
-        window.set_framebuffer_size_polling(true);
-
-        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-        let mut count = 0;
-
-        while !window.should_close() {
-            count += 1;
-            report_run_count(count);
-            window.swap_buffers();
-            glfw.poll_events();
-
-            for (_, event) in glfw::flush_messages(&events) {
-                println!("{:?}", event);
-                match event {
-                    glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                        window.set_should_close(true)
-                    },
-                _ => {},
-            }
-        }
-    }
+    start_window_manager();
 }
--- src/os/window_manager.rs
+++ src/os/window_manager.rs
@@ -1,63 +1,46 @@
+use glfw::Action;
+use glfw::{Context, Key};
 
-use winit::event_loop::EventLoop;
-use winit::window::WindowBuilder;
-use winit::event::Event;
-use winit::event::WindowEvent;
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
 
-use crate::graphics::opengl::render;
+fn report_run_count(count: i32) {
+    println!("Running... Count {}", count)
+}
 
-// TODO: revert to glfw
-pub fn updateWindow() {
-    println!("rendering graphics...");
+pub fn start_window_manager() {
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
 
-    let event_loop = EventLoop::new();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
 
-    let window = WindowBuilder::new()
-        .with_title("Gamengine")
-        .with_inner_size(winit::dpi::LogicalSize::new(800.0, 600.0))
-        .build(&event_loop)
-        .unwrap();
+    let (mut window, events) =
+        glfw.create_window(SCREEN_WIDTH, SCREEN_HEIGHT, "GamEngine", glfw::WindowMode::Windowed)
+            .expect("Failed to create GLFW window");
 
-    event_loop.run(move |event, _, control_flow| {
-         // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
-        // dispatched any events. This is ideal for games and similar applications.
-        control_flow.set_poll();
+        window.make_current();
+        window.set_key_polling(true);
+        window.set_framebuffer_size_polling(true);
 
-        // ControlFlow::Wait pauses the event loop if no events are available to process.
-        // This is ideal for non-game applications that only update in response to user
-        // input, and uses significantly less power/CPU time than ControlFlow::Poll.
-        // control_flow.set_wait();
-        
-        // println!("{event:?}");
+        gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
 
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("The close button was pressed; stopping");
-                control_flow.set_exit();
-            },
-            Event::MainEventsCleared => {
-                // Application update code.
-    
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need to redraw, in
-                // applications which do not always need to. Applications that redraw continuously
-                // can just render here instead.
-                render();
-                window.request_redraw();
-            },
-            Event::RedrawRequested(_) => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render continuously to render in
-                // this event rather than in MainEventsCleared, since rendering in here allows
-                // the program to gracefully handle redraws requested by the OS.
-            },
-            _ => ()
+        let mut count = 0;
+
+        while !window.should_close() {
+            count += 1;
+            report_run_count(count);
+            window.swap_buffers();
+            glfw.poll_events();
+
+            for (_, event) in glfw::flush_messages(&events) {
+                println!("{:?}", event);
+                match event {
+                    glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                        window.set_should_close(true)
+                    },
+                _ => {},
+            }
         }
-    });   
-
+    }
 }

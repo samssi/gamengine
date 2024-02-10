a9ef9e0f1da14e873591f39bb8b8ce722605913f
samssi <samir.hamadi@gmail.com>
2024-02-11 00:46:11 +0200

    Add init function

--- src/game/game.rs
+++ src/game/game.rs
@@ -1,4 +1,4 @@
-use glfw::Key;
+use glfw::{GlfwReceiver, Key, WindowEvent};
 use crate::entity::entity::{Entity3d, Shading, TRIANGLE};
 use crate::game::keyboard_handler::glfw_press_handler;
 use crate::graphics::opengl::create_shader_programs;
@@ -12,7 +12,7 @@ fn game_render_event(game_context: &mut GameContext) {
     rotation.z = rotation.z + 0.01;
 }
 
-pub fn start() {
+fn init_game() -> (GameContext, GlfwReceiver<(f64, WindowEvent)>) {
     let keymap = create_keymap();
     let basic_shading = Shading{
         vertex_shader: String::from("basic.vert"),
@@ -35,13 +35,16 @@ pub fn start() {
         keymap
     };
 
-    let game_context = GameContext {
+    (GameContext {
         shader_context,
         window_context,
         entity_context,
         keyboard_context
-    };
+    }, events)
+}
 
+pub fn start() {
+    let (game_context, events) = init_game();
     start_opengl_window_manager(
         game_context,
         events,

b30cc968579942c27e0a5707458a380be72db699
samssi <samir.hamadi@gmail.com>
2024-02-11 23:28:17 +0200

    Add object conversion to triangle points

--- src/io/object.rs
+++ src/io/object.rs
@@ -1,7 +1,7 @@
-struct WavefrontObject {
+pub struct WavefrontObject {
     o: String,
     v: Vec<Vec<f32>>,
-    f: Vec<Vec<Vec<f32>>>
+    f: Vec<Vec<Vec<i32>>>
 }
 
 fn push_to_acc<'a>(pattern: &str, value: &'a str, acc: &mut Vec<&'a str>) {
@@ -28,7 +28,7 @@ fn parse_f(fs: Vec<&str>) -> Vec<Vec<Vec<i32>>> {
     faces
 }
 
-pub fn parse_points_from_object_file(file_content: &String) /*-> WavefrontObject*/ {
+pub fn parse_points_from_object_file(file_content: &String) -> WavefrontObject {
     let (o, v, vt, f, vn): (Vec<&str>, Vec<&str>, Vec<&str>, Vec<&str>, Vec<&str>) = file_content
         .lines()
         .fold( (vec![], vec![], vec![], vec![], vec![]), |mut acc, value| {
@@ -42,6 +42,19 @@ pub fn parse_points_from_object_file(file_content: &String) /*-> WavefrontObject
 
     let parsed_v = parse_v(v);
     let parsed_f = parse_f(f);
-    println!("{:?}", parsed_v);
-    println!("{:?}", parsed_f);
+
+    WavefrontObject{
+        o: String::from(o[0]),
+        v: parsed_v,
+        f: parsed_f
+    }
+}
+
+pub fn wavefront_object_as_triangle_points(wavefront_object: WavefrontObject) -> Vec<Vec<f32>> {
+    let elements: Vec<Vec<i32>> = wavefront_object.f.to_vec().into_iter().flatten().collect();
+    let positions: Vec<i32> = elements.into_iter().map(|element| element[0] - 1).collect();
+    positions.into_iter().map(|position| {
+        let triangles =  &wavefront_object.v[position as usize];
+        triangles.to_vec()
+    }).collect()
 }

7785f97914ae2807c2183915fbad70c1f33a398b
samssi <samir.hamadi@gmail.com>
2024-02-18 19:44:30 +0200

    Change cursor handling as event based

--- src/os/window_manager.rs
+++ src/os/window_manager.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
 use glfw::{Context, GlfwReceiver, Key, PWindow, Window, WindowEvent};
 use glfw::ffi::GLFWwindow;
+use crate::game::window_handler::glfw_cursor_handler;
 
 use crate::graphics::opengl::{create_program, create_shader_programs, init_renderer, link_shaders, render};
 use crate::io::keyboard::{handle_keyboard_events, KeyActivity};
@@ -14,14 +15,25 @@ fn process_events<T> (
     ) {
     for (_, event) in glfw::flush_messages(&events) {
         match event {
+            WindowEvent::CursorPos(x_pos, y_pos) => {
+                glfw_cursor_handler(game_context, x_pos, y_pos)
+            },
             WindowEvent::Key(key, _, action, _) => {
                 handle_keyboard_events(game_context, key, action, glfw_press_handler)
             }
         _ => {},
         }
     }
-
 }
+
+fn process_cursor<T>(
+    game_context: &mut GameContext<T>) {
+    if game_context.window_context.window.is_hovered() {
+        let (x_pos, y_pos) = game_context.window_context.window.get_cursor_pos();
+        println!("{}, {}", x_pos, y_pos);
+    }
+}
+
 
 pub fn init_opengl_window_manager() -> (WindowContext, GlfwReceiver<(f64, WindowEvent)>) {
     let window_properties = WindowProperties{
@@ -41,6 +53,7 @@ pub fn init_opengl_window_manager() -> (WindowContext, GlfwReceiver<(f64, Window
 
     window.make_current();
     window.set_key_polling(true);
+    window.set_cursor_pos_polling(true);
     window.set_framebuffer_size_polling(true);
 
     gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
@@ -57,7 +70,7 @@ pub fn start_opengl_window_manager<'a, T>(
     events: GlfwReceiver<(f64, WindowEvent)>,
     game_render_event: fn(game_context: &mut GameContext<T>),
     glfw_press_handler: fn(game_context: &mut GameContext<T>, key: Key),
-    glfw_window_handler: fn(&mut Window, f64, f64)) {
+    glfw_window_handler: fn(game_context: &mut GameContext<T>, f64, f64)) {
     println!("Starting window manager!");
 
     // TODO: handle error if time goes backwards
@@ -76,7 +89,7 @@ pub fn start_opengl_window_manager<'a, T>(
         game_context.window_context.window.swap_buffers();
         game_context.window_context.glfw.poll_events();
 
-        game_context.window_context.window.set_cursor_pos_callback(glfw_window_handler);
+        // process_cursor(&mut game_context);
         process_events(&mut game_context, &events, glfw_press_handler)
     }
 }
